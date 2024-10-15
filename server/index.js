14537d6833c019e2af9ea87087e8047717d051f7
Rahul Shah <rahulshah0728@gmail.com>
2024-10-15 22:02:55 +0530

    First commit for my ProjectFlow Project which manage the relation of cilent and project

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const port = process.env.PORT;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+} );
+
+app.use(
+  '/graphql', 
+  graphqlHTTP({
+  schema,
+  graphiql: process.env.NODE_ENV === 'development'
+}))
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
